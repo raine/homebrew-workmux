963ab138d6243aef8b97bfa0eabf778af0a55969
Raine Virta <raine.virta@gmail.com>
2025-11-25 18:44:58 +0200

    init

---
+++ Formula/workmux.rb
@@ -0,0 +1,10 @@
+class Workmux < Formula
+  desc "Opinionated workflow tool that orchestrates git worktrees and tmux"
+  homepage "https://github.com/raine/workmux"
+  version "0.0.0"
+  license "MIT"
+
+  def install
+    # Placeholder - will be replaced by CI
+  end
+end
